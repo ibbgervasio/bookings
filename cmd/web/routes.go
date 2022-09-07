80ad8e99f8425158d45e41f704133879bc44e596
Iumi Gervasio <ibbgervasio@gmail.com>
2022-09-07 21:14:39 +0800

    Add static directory with images and changes to home.page.tmpl

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -23,6 +23,10 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
+	//place to go get static files
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return mux
 
 }
